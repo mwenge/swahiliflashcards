51b4a3d45a1e9828528e8b913acef7fb3cc2d838
Robert Hogan <robhogan@gmail.com>
2021-10-24 22:42:07 +0100

    Show translatio in same page

--- flashwords.js
+++ flashwords.js
@@ -2,15 +2,26 @@ function* makeShuffleGenerator(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
-    yield array[i][0];
-    yield array[i][1];
+    yield array[i];
   }
   return;
 }
 
 const w = makeShuffleGenerator(words);
-let we = document.getElementById("word");
-we.textContent = w.next().value;
-we.onclick = function () {
-  we.textContent = w.next().value;
+let ws = document.getElementById("swahili");
+let we = document.getElementById("english");
+let [swahili, english] = w.next().value;
+we.style.visibility = 'hidden';
+we.textContent = english;
+ws.textContent = swahili;
+
+document.body.onclick = function () {
+  if (we.style.visibility == 'hidden') {
+    we.style.visibility = 'visible';
+    return;
+  }
+  let [swahili, english] = w.next().value;
+  we.style.visibility = 'hidden';
+  we.textContent = english;
+  ws.textContent = swahili;
 };
